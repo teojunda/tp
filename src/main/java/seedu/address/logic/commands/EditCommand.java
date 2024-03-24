927e29d59ff8a034fd6525e666cb5e771a8374d4
teojunda <teojunda@gmail.com>
2024-03-25 00:13:11 +0800

    Fix test cases

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -162,6 +162,7 @@ public class EditCommand extends Command {
          * Creates a new EditPersonDescriptor with empty fields.
          */
         public EditPersonDescriptor() {
+            phone = Optional.empty();
             telegram = Optional.empty();
             github = Optional.empty();
         }
